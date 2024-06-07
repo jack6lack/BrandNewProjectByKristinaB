6a77579f0ef3def72f46d7abbf7760b425092bf9
jack6lack <jack6lack@gmail.com>
2024-06-07 16:42:17 +0300

    4 задание (в процессе)

---
+++ src/ru/innopolis/java/fourth/AlsoMain.java
@@ -0,0 +1,11 @@
+package ru.innopolis.java.fourth;
+
+import java.util.Scanner;
+
+public class AlsoMain {
+    String[] letters = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
+    Scanner scanner = new Scanner(System.in);
+    String theLetter = scanner.next();
+//    for(String )
+
+}
