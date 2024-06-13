d58e442dd69e99b36da3355e2a94c3ced2014fcd
jack6lack <jack6lack@gmail.com>
2024-06-13 15:08:26 +0300

    4 задание (переделано)

--- src/ru/innopolis/java/third/App.java
+++ src/ru/innopolis/java/third/App.java
@@ -8,28 +8,60 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        TV tv1 = new TV();
+        Television tv1 = new Television();
         tv1.setModel("sumsuk");
         tv1.setSmart(true);
         tv1.setDiscount(15);
-        tv1.setPrice(100_000);
+        tv1.setPriceWithoutDiscount(100_000);
         System.out.println(tv1);
 
-        TV tv2 = new TV("lgeee", 30_000, 0, false);
+        Television tv2 = new Television("lgeee", 30_000, 0, false);
         System.out.println(tv2);
 
-        TV tv3 = new TV("xixiomi", 49_999, 4.99, true);
+        Television tv3 = new Television("xixiomi", 49_999, 4.99, true);
         System.out.println(tv3);
 
-        TV tv4 = new TV(scanner);
+        Television tv4 = new Television();
+        System.out.println("введите желаемую модель: ");
+        tv4.setModel(scanner.next());
+        System.out.println("введите желаемую цену: ");
+        tv4.setPriceWithoutDiscount(scanner.nextDouble());
+        System.out.println("введите желаемую скидку: ");
+        tv4.setDiscount(scanner.nextDouble());
+        System.out.println("вам необходимы смарт функции? ( y / n )");
+        if (scanner.next().equals("y")) {
+            tv4.setSmart(true);
+        } else {
+            tv4.setSmart(false);
+        }
+        System.out.println("ваша итоговая цена: " + tv4.getFinalPrice());
         System.out.println(tv4);
 
-        TV tv5 = new TV(random, scanner);
-        if(tv5.getModel()==null){
+        Television tv5 = new Television();
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println("хотите случайный телевизор? (y / n)");
+        String st = scanner.next();
+        if (st.equals("y")) {
+            for (int i = 0; i < random.nextInt(5, 26); i++) {
+                stringBuilder.append(tv5.getAlphabet()[random.nextInt(26)]);
+            }
+            tv5.setModel(stringBuilder.toString());
+            System.out.println("полученная модель: " + tv5.getModel());
+            tv5.setPriceWithoutDiscount(Math.abs(random.nextInt(1_000, 1_000_000)));
+            System.out.println("полученная цена: " + tv5.getPriceWithoutDiscount());
+            tv5.setDiscount(Math.abs(random.nextInt(100)));
+            System.out.println("полученная скидка: " + tv5.getDiscount());
+            System.out.println("ваша итоговая цена: " + tv5.getFinalPrice());
+            tv5.setSmart(random.nextBoolean());
+        } else if (st.equals("n")) {
+            System.out.println("ну и ладно, нет так нет");
+        } else {
+            System.out.println("в следующий раз читайте внимательнее, магии не будет");
+        }
+        if (tv5.getModel() == null) {
             System.out.println("пака");
         } else {
             System.out.println(tv5);
         }
-
     }
 }
---
+++ src/ru/innopolis/java/third/Television.java
@@ -0,0 +1,79 @@
+package ru.innopolis.java.third;
+
+public class Television {
+    private String model;
+    private double priceWithoutDiscount;
+    private double finalPrice;
+    private double discount;
+    private boolean isSmart;
+    private final String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    //если юзер не знает какой телек
+    public Television() {
+    }
+
+    //если юзер знает
+    public Television(String model, double priceWithoutDiscount, double discount, boolean isSmart) {
+        this.model = model;
+        this.discount = discount;
+        this.priceWithoutDiscount = priceWithoutDiscount;
+        this.isSmart = isSmart;
+        this.finalPrice = getFinalPrice();
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPriceWithoutDiscount() {
+        return priceWithoutDiscount;
+    }
+
+    public void setPriceWithoutDiscount(double priceWithoutDiscount) {
+        this.priceWithoutDiscount = priceWithoutDiscount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        if (discount > 100) {
+            System.out.println("больше всего в этой жизни вас радует то, что воздух бесплатный. ваша скидка будет 0%");
+            this.discount = 0;
+        } else {
+            this.discount = discount;
+        }
+    }
+
+    public boolean isSmart() {
+        return isSmart;
+    }
+
+    public void setSmart(boolean smart) {
+        this.isSmart = smart;
+    }
+
+    public double getFinalPrice() {
+        return this.priceWithoutDiscount - (this.priceWithoutDiscount / 100 * this.discount);
+    }
+
+    public String[] getAlphabet() {
+        return alphabet;
+    }
+
+    @Override
+    public String toString() {
+        return "TV <" +
+                " модель '" + model + '\'' +
+                ", цена " + priceWithoutDiscount + " р." +
+                ", скидка составляет: " + discount + "%" +
+                ", ваша финальная цена составляет: " + getFinalPrice() + " p." +
+                (isSmart ? ", смарт тв" : ", не смарт тв") +
+                '>';
+    }
+}
