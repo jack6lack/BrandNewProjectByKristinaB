693ef6d4085909e2c29e69d37ef6d5df5bdad02d
jack6lack <jack6lack@gmail.com>
2024-06-21 15:01:38 +0300

    сразу три момента: 1. был использован лист продуктов вместо массива, потому что с массивом получается слишком много некрасивого кода (проверка размера, добавление, увеличение размера при каждом добавлении (иначе выводятся null, если поставить заочно размер поболбше)) 2. вводить "человек" и "продукт" в App нужно в две строки, а не одну, как в примере, потому что иначе возникает много проблем, next() подбирает следующее слово вне зависимости от того, часть имени это или уже продукт. можно решить, конечно, проверками, но это тоже уродливо 3. Person было добавлено поле gender, потому что, согласно примеру, должно выводиться "купил" или "купила", в зависимости от пола
    из важного: toString() переопределен именно так, чтобы соответствовать ТЗ. выводятся не все поля таким образом.
    
    порядок работы:
    1. созданы два листа, один хранит данные покупателей, второй - доступные продукты.
    2. создаются две переменные типа String, которые будут хранить строки, которые печатает пользователь с клавиатуры в консоль
    3. цикл do-while отвечает за подбор введенных в консоль строк, собирает все покупки, пока не введено END
    4. первый цикл for-each проверяет, существует ли введенный в консоль человек в personList
    5. вложенный цикл for-each проверяет, существует ли введенный продукт в productList, и, если все звезды совпали, выполняется метод addinToShopper, который вызывает метод buyinForMoney (проверяет платежеспособность и отнимает деньги), добавляет в пакет товары и выводит в консоль сообщение об успехе/неудаче
    6. после выводятся в консоль все покупатели и все их покупки (тоже циклом, потому что toString() выведет их в одну строку, а я хочу на каждой строке видеть каждого отдельного покупателя)
    
    как-то так

--- src/ru/innopolis/java/homeworks/homework06/App.java
+++ src/ru/innopolis/java/homeworks/homework06/App.java
@@ -1,7 +1,75 @@
 package ru.innopolis.java.homeworks.homework06;
 
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Person> personList = new ArrayList<>();
+        personList.add(new Person("Павел Андреевич", 10_000, 'm'));
+        personList.add(new Person("Анна Петровна", 2_000, 'f'));
+        personList.add(new Person("Борис", 10, 'm'));
+        personList.add(new Person("Женя", 0, 'f'));
+        personList.add(new Person("Света", -3, 'f'));
+
+        ArrayList<Product> productList = new ArrayList<>();
+        productList.add(new Product("Хлеб", 40));
+        productList.add(new Product("Молоко", 60));
+        productList.add(new Product("Торт", 1000));
+        productList.add(new Product("Кофе растворимый", 879));
+        productList.add(new Product("Масло", 150));
+        productList.add(new Product("Мороженое", 200));
+        productList.add(new Product("Макароны", 800));
+
+        String checkNameOrEnd;
+        String checkProduct = "";
+
+        System.out.println("начинаем покупки: ");
+        do {
+//            checkNameOrEnd = scanner.nextLine().replaceAll("\\s", "").toLowerCase();
+            checkNameOrEnd = scanner.nextLine();
+//            checkProduct = scanner.nextLine().replaceAll("\\s", "").toLowerCase();
+            if (!checkNameOrEnd.equals("END")) {
+                checkProduct = scanner.nextLine();
+            }
+            for (Person person : personList) {
+                if (person.getName().equals(checkNameOrEnd)) {
+                    for (Product product : productList) {
+                        if (product.getNameOfProduct().equals(checkProduct)) {
+                            addinToShopper(person, product);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        } while (!Objects.equals(checkNameOrEnd, "END"));
+        for (Person person : personList) {
+            System.out.println(person);
+        }
+    }
+
+    static boolean buyinForMoney(Person person, Product product) {
+        int thinnerCash = person.getCash() - product.getPrice();
+        if (thinnerCash < 0) {
+            return false;
+        } else {
+            person.setCash(thinnerCash);
+            return true;
+        }
+    }
+
+    static void addinToShopper(Person person, Product product) {
+        if (buyinForMoney(person, product)) {
+            person.addToShopper(product, person.getShopper());
+            System.out.println(person.getName() + (person.getGender() == 'm' ? " купил " : " купила ") + product.getNameOfProduct());
+        } else {
+            System.out.println(person.getName() + " не может позволить себе " + product.getNameOfProduct());
+        }
     }
 }
