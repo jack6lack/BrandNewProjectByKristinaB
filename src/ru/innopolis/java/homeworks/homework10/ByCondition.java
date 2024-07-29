ff62949b1de83684742a844d033475cdc6c92bd3
Chris <jack6lack@gmail.com>
2024-07-29 18:16:26 +0300

    1. функциональный интерфейс ByCondition с единственным (ну конечно же) методом isOk, принимающим целое число 2. класс Sequence с методом filter, возвращающим массив целых чисел. принимает изначальный массив и экземпляр ByCondition. проходит по масссссиву в цикле и вызывает метод isOk, в который передается каждый элемент массива. если isOk вернул true, то данный элемент добавляется в новый массив, который и вернется после выполнения метода 3. в мейне в лямбде задается условие для isOk, с которым потом и фильтруем массив (ну и выясняем четность/нечетность суммы) 4. дженериковый Pair - аналог Optional создан, надеюсь, фразу "похожий на Optional и не запрещающий элементам принимать значение null" я поняла верно. требуемые тесты проходит.

---
+++ src/ru/innopolis/java/homeworks/homework10/ByCondition.java
@@ -0,0 +1,6 @@
+package ru.innopolis.java.homeworks.homework10;
+
+@FunctionalInterface
+public interface ByCondition {
+    boolean isOk(int number);
+}
---
+++ src/ru/innopolis/java/homeworks/homework10/Pair.java
@@ -0,0 +1,37 @@
+package ru.innopolis.java.homeworks.homework10;
+
+import java.util.Objects;
+
+public class Pair<T, V> {
+    private final T first;
+    private final V second;
+
+    private Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return this.first;
+    }
+
+    public V getSecond() {
+        return this.second;
+    }
+
+    public static <T, V> Pair<T, V> of(T first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair<?, ?> pair)) return false;
+        return Objects.equals(getFirst(), pair.getFirst()) && Objects.equals(getSecond(), pair.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirst(), getSecond());
+    }
+}
