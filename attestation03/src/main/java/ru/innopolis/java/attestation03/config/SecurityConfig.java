d4c9ddbbe351ce8f26b870aea52e1911519eb4ff
Chris <jack6lack@gmail.com>
2024-10-09 22:25:50 +0300

    1. Создание основного функционала:  Сущности: User, Result, Game, Characteristics и их связи.  Репозитории: для доступа к данным через CrudRepository.  Сервисы: добавление бизнес-логики для работы с сущностями.  DTO: для передачи данных между слоями. 2. Настройка базы данных:  Использование PostgreSQL, настройка Flyway для миграций.  Автоматическая генерация данных (например, через имена персонажей TES). 3. Docker: настройка контейнеров для запуска базы данных и приложения. 4. Swagger UI: подключение для документирования API. 5. Тестирование:  Написание юнит-тестов для сервисов.  Использование моков для тестирования зависимостей. 6. Авторизация с помощью Spring Security (создан единственный user с правами USER) 7. Дополнительно:  Настройка мягкого удаления записей (soft delete).  Enum для перечисления возможных результатов игр

---
+++ attestation03/src/main/java/ru/innopolis/java/attestation03/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package ru.innopolis.java.attestation03.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/api/users", true)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
